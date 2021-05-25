5b810c16f60355c07ea1f6de2cef9bdbffb3870a
ArthurDev7 <arthurluizdev7@gmail.com>
2021-05-25 06:47:44 -0300

    Calculo Circuferencia

---
+++ src/CalculoCircuferencia.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CalculoCircuferencia {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o Raio:");
+        double raio = scan.nextDouble();
+
+        double area = 3.14159 * (Math.pow(raio,2));
+
+        System.out.printf("A=%.4f\n", area);
+    }
+}
